6fba2db72608a2ef2c5422fb5e9d5cfbdb76705c
Florian Schaller <florian.schaller@heig-vd.ch>
2018-07-27 15:54:15 +0200

    Upload APIDOC on git account

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Email-Engine-API",
+  "version": "1.0.0",
+  "description": "Creation of an API for WIDE Agency email engine",
+  "title": "Email Engine REST API",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-07-27T13:50:52.435Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
